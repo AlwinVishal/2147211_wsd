f7e871db7dd13e56fa249f8703f38021c275af42
AlwinVishal <alwin.j@mca.christuniversity.in>
2022-02-15 22:50:19 +0530

    *

--- lab08.js
+++ lab08.js
@@ -8,7 +8,7 @@ app.controller('mainCtrl', function($scope,$http){
     $scope.rowlimit=4;
     $scope.fullName ;
     $scope.today = new Date();
-    $http.get('https://v-mani.github.io/2147122_Manibharathy_V/JSON/lab08.json')
+    $http.get('https://alwinvishal.github.io/2147211_wsd/lab08.json')
     .success(function(response)
     {
         $scope.names=response.employees;
